ef68f7c2cf116b6103158296391ffed24caf3999
Adhiraj-11 <adhirajsingh.chauhan2021@vitstudent.ac.in>
2023-06-03 03:03:33 +0530

    till 3.03AM

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # for w in words:
-    #     bag.append(1) if w in pattern_words else bag.append(0)
-
-    # output_row = list(output_empty)
-    # output_row[tags.index(doc[1])] = 1
+# print(intents)
+# try:
+#     with open("data.pick",'rb') as f:
+#         words,tags,training,output = pickle.load()
+# except:
--- train.py
+++ train.py
@@ -17,94 +17,96 @@ import json
 with open('intents.json') as json_data:
     intents = json.load(json_data)
 
-print(intents)
-try:
-    with open("data.pick",'rb') as f:
-        words,tags,training,output = pickle.load()
-except:
-    words = []
-    tags = []
-    documents = []
-    ignore = ['?','!','.',',']
+# print(intents)
+# try:
+#     with open("data.pick",'rb') as f:
+#         words,tags,training,output = pickle.load()
+# except:
+words = []
+tags = []
+documents = []
+ignore = ['?','!','.',',']
 
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            w = tokenize(pattern)
-            words.extend(w)
-            documents.append((w, intent['tag']))
-            if intent['tag'] not in tags:
-                tags.append(intent['tag'])
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in tags:
+            tags.append(intent['tag'])
 
-    words = [stem(w) for w in words if w not in ignore]
-    words = sorted(list(set(words)))
-    tags = sorted(list(set(tags)))
+words = [stem(w) for w in words if w not in ignore]
+words = sorted(list(set(words)))
+tags = sorted(list(set(tags)))
 
-    # print(tags)
-    # print (len(documents), "documents")
-    # print (len(tags), "classes", tags)
-    # print (len(words), "unique stemmed words", words)
-
-
-    training = []
-    output = []
-
-    output_empty = [0] * len(tags)
+# print(tags)
+# print (len(documents), "documents")
+# print (len(tags), "classes", tags)
+# print (len(words), "unique stemmed words", words)
 
 
-    for doc in documents:
-    
-        bag = []
-        pattern_words = doc[0]
-        # bag_of_words(tokenized_sentence, words)
-        pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-        for w in words:
-            bag.append(1) if w in pattern_words else bag.append(0)
+training = []
+output = []
 
-        output_row = list(output_empty)
-        output_row[tags.index(doc[1])] = 1
+output_empty = [0] * len(tags)
 
 
-        training.append([bag, output_row])
+for doc in documents:
 
-    random.shuffle(training)
-    training = np.array(training)
+    bag = []
+    pattern_words = doc[0]
+    # bag_of_words(tokenized_sentence, words)
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[tags.index(doc[1])] = 1
 
 
-    train_x = list(training[:,0])
-    train_y = list(training[:,1])
+    training.append(bag)
+    output.append(output_row)
 
-    with open("data.pick",'wb') as f:
-        pickle.dump((words,tags,training,output),f)
+#random.shuffle(training)
+training = np.array(training)
+output = np.array(output)
+
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+with open("data.pick",'wb') as f:
+    pickle.dump((words,tags,training,output),f)
 
 
 net = model_(train_x,train_y)
 
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-# model.save('model.tflearn')
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
 
-try:
-    model.load("model.tflearn")
-except:
+# try:
+#     model.load("model.tflearn")
+# except:
 
-# Start training (apply gradient descent algorithm)
-    model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save('model.tflearn')
+# # Start training (apply gradient descent algorithm)
+#     model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+#     model.save('model.tflearn')
 
-def chat():
-    print("start talking!")
-    flag = True
-    while(flag):
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            flag = False
+# def chat():
+#     print("start talking!")
+#     flag = True
+#     while(flag):
+#         inp = input("You: ")
+#         if inp.lower() == "quit":
+#             flag = False
 
-        results = model.predict([bag_of_words(inp,words)])
-        results_index = np.argmax(results)
-        tag_temp = tags[results_index]
-        for tg in intents["intents"]:
-            if tg ['tag'] == tag_temp: 
-                responses = tg['responses']
+#         results = model.predict([bag_of_words(inp,words)])
+#         results_index = np.argmax(results)
+#         tag_temp = tags[results_index]
+#         for tg in intents["intents"]:
+#             if tg ['tag'] == tag_temp: 
+#                 responses = tg['responses']
                  
-        print(random.choice(responses)) 
-chat()
+#         print(random.choice(responses)) 
+# chat()
